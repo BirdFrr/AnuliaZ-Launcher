081d90492c3bb0f00d658a8a11cfb46261dff3fa
BirdFrr <163045918+BirdFrr@users.noreply.github.com>
2024-03-12 09:33:48 +0100

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,44 +1,16 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.8.9",
-        "loadder_type" => "forge",
-        "loadder_version" => "latest"
-    ),
-    "verify" => true,
-    "ignored" => array(
-        'config',
-        'essential',
-        'logs',
-        'resourcepacks',
-        'saves',
-        'screenshots',
-        'shaderpacks',
-        'W-OVERFLOW',
-        'options.txt',
-        'optionsof.txt'
-    ),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
-    )
-));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
+$instance['anuliaz'] = array_merge($instance['anuliaz'], array(
     "loadder" => array(
         "minecraft_version" => "1.16.5",
         "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
+        "loadder_version" => "1.16.5-36.2.34"
     ),
     "verify" => false,
     "ignored" => array(),
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "PokeMoonX",
+        "nameServer" => "anuliaz",
         "ip" => "178.32.106.234",
         "port" => 25599
     )
